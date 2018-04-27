bdbbf8b0e88f28901c6049fe9f6abd883dd9587d
persilee <persilee@foxmail.com>
2018-04-27 23:31:27 +0800

    modify mail tpl

--- utilities/send-mail.js
+++ utilities/send-mail.js
@@ -46,29 +46,44 @@ exports.notice = (comment) => {
 }
 
 exports.send = (currentComment, parentComment) => {
-    let emailSubject = '👉 叮咚！「' + process.env.SITE_NAME + '」上有人@了你';
-    let emailContent = '<div style="background-color:white;border-top:2px solid #12ADDB;box-shadow:0 1px 3px #AAAAAA;line-height:180%;padding:0 15px 12px;width:500px;margin:50px auto;color:#555555;font-family:\'Century Gothic\',\'Trebuchet MS\',\'Hiragino Sans GB\',微软雅黑,\'Microsoft Yahei\',Tahoma,Helvetica,Arial,\'SimSun\',sans-serif;font-size:12px;">  \n' +
-        '    <h2 style="border-bottom:1px solid #DDD;font-size:14px;font-weight:normal;padding:13px 0 10px 8px;"><span style="color: #12ADDB;font-weight:bold;">&gt; </span>您（' +
-        parentComment.get('nick') +
-        '）在 <a href="' + process.env.SITE_URL + '">《' + process.env.SITE_NAME + '》</a> 上的评论有了新的回复</h2>' +
-        '<div style="padding:0 12px 0 12px;margin-top:18px">' +
-        '<p>你的评论：</p>' +
-        '<div style = "background-color: #f5f5f5;padding: 10px 15px;margin:18px 0;word-wrap:break-word;" > ' +
-        parentComment.get('comment') +
-        '</div><p><strong>' +
-        currentComment.get('nick') +
-        '</strong>&nbsp;@了你，回复内容如下：</p>' +
-        '<div style = "background-color: #f5f5f5;padding: 10px 15px;margin:18px 0;word-wrap:break-word;" > ' +
-        currentComment.get('comment') +
-        '</div>' +
-        '<p>您可以点击 <a href="' +
+    let emailSubject = '👉 叮咚！[' + process.env.SITE_NAME + ']上的留言有了回应';
+    let emailContent = '<div style="background: white; width: 80%; margin: auto auto; border-radius: 5px; border:orange 1px solid; overflow: hidden; -webkit-box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.12); box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.18);">' +
+        '<span>' +
+        '<a href="#">' +
+        '<img style="width:100%;z-index: 666;" src="https://lishaoy.net/images/boy.jpg" />' +
+        '</a>' +
+        '</span>' +
+        '<div style="padding: 5px 20px;">' +
+        '<p style="position: relative; color: white; float: left; z-index: 999; background: orange; padding: 5px 30px; margin: -25px auto 0 ; box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.30)">' +
+        '<span>Dear' + currentComment.get('nick') +
+        '</span>' +
+        '</p>' +
+        '<span><br / ></span>' +
+        '<h3> <span > 您有一条来自' +
+        '<a style="text-decoration: none;color: orange" target="_blank" href="' +
         process.env.SITE_URL +
-        currentComment.get('url') +
-        '">查看回复完整内容</a>，欢迎再次光临 <a href="' +
-        process.env.SITE_URL + '">' +
-        process.env.SITE_NAME + ' </a> 。<p>本邮件为系统自动发送，请勿直接回复。</p>' +
-        '</div>' +
-        '</div>';
+        '">' + parentComment.get('nick') + '</a>的回复</span>' +
+        '</h3>' +
+        '<span>< br /></span >' +
+        '<p style="font-size: 14px;">< span > 您在《' +
+        process.env.SITE_NAME +
+        '》上发表的评论：</span></p >' +
+        '<p style="border-bottom:#ddd 1px solid;border-left:#ddd 1px solid;padding-bottom:20px;background-color:#eee;margin:15px 0px;padding-left:20px;padding-right:20px;border-top:#ddd 1px solid;border-right:#ddd 1px solid;padding-top:20px">' +
+        parentComment.get('comment') +
+        '</p>' +
+        '<p style="font-size: 14px;">' +
+        '<span>' + currentComment.get('nick') + '给您的回复如下：</span></p>' +
+        '<p style="border-bottom:#ddd 1px solid;border-left:#ddd 1px solid;padding-bottom:20px;background-color:#eee;margin:15px 0px;padding-left:20px;padding-right:20px;border-top:#ddd 1px solid;border-right:#ddd 1px solid;padding-top:20px">' +
+        currentComment.get('comment') +
+        '</p>' +
+        '<p style="font-size: 14px;"><span><a style="text-decoration: none;color: orange" target="_blank" href="' +
+        process.env.SITE_URL +
+        '">persilee\'s blog</a>双手呈上~</span ></p>' +
+        '<div style="text-align: center;"><span><img src="https://i.loli.net/2018/03/19/5aafd2e0ae335.png" alt="hr" style="max-height:30px;margin:5px auto 5px auto;display: block;"/><a style="text-transform: uppercase; text-decoration: none; font-size: 14px; border: 2px solid #6c7575; color: #2f3333; padding: 10px; display: inline-block; margin: 10px auto 0;"target="_blank" href="' +
+        process.env.SITE_URL + currentComment.get('url') +
+        '">点击查看回复的完整內容</a></span></div>' +
+        '<p style="font-size: 14px;text-align: center;"><span>本邮件为系统自动发出，请勿直接回复</span></p><p style="font-size: 12px;text-align: center;color: #999;"><span>Copyright © persilee\'s blog</span></p>' +
+        '</div></div>';
 
     let mailOptions = {
         from: '"' + process.env.SENDER_NAME + '" <' + process.env.SENDER_EMAIL + '>', // sender address
