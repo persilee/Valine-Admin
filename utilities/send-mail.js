8235867537ef89f70d94fba537a5178d45c2ebc9
persilee <i@lishaoy.net>
2020-05-29 10:33:35 +0800

    🍉 modify mail

--- utilities/send-mail.js
+++ utilities/send-mail.js
@@ -12,28 +12,27 @@ const transporter = nodemailer.createTransport({
 
 exports.notice = (comment) => {
     let emailSubject = '👉 咚！「' + process.env.SITE_NAME + '」上有新评论了';
-    let emailContent = '<div style="background: white; max-width:740px; margin: auto auto; border-radius: 5px; border:#87daff 1px solid; overflow: hidden; -webkit-box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.12); box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.18);">' +
+    let emailContent = '<div style="background: white; max-width:740px; margin: 10px auto; border-radius: 5px; border:#FF5722 1px solid; overflow: hidden; -webkit-box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.12); box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.18);">' +
         '<a href="#">' +
         '<img height="auto" style="width:140%;max-width:740px;z-index: 666;" src="https://cdn.lishaoy.net/images/boy.png"/>' +
         '</a>' +
         '<div style="padding: 5px 20px;">' +
-        '<div style="position: relative; color: white; float: left; z-index: 999; background: #87daff; padding: 5px 30px; margin: -25px auto 0 ; box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.30)">' +
-        '<span>Dear Persilee'  +
-        '</span>' +
+        '<div style="position: relative; color: white; float: left; z-index: 999; background: #FF5722; padding: 5px 30px; margin: -25px auto; box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.30)">' +
+        'Dear Persilee' +
         '</div>' +
         '<span><br / ></span>' +
         '<h3> <span > 您有一条来自' +
-        '<a style="text-decoration: none;color: #87daff" target="_blank" href="' +
+        '<a style="text-decoration: none;color: #FF5722" target="_blank" href="' +
         process.env.SITE_URL +
         '">' + comment.get('nick') + '</a>的评论</span>' +
         '</h3>' +
         '<p style="font-size: 14px;"><span> 您在《' +
         process.env.SITE_NAME +
         '》上发表的评论：</span></p >' +
-        '<div style="border-bottom:#ddd 1px solid;border-left:#ddd 1px solid;padding-bottom:20px;background-color:#eee;margin:15px 0px;padding-left:20px;padding-right:20px;border-top:#ddd 1px solid;border-right:#ddd 1px solid;padding-top:20px">' +
+        '<div style="border-bottom:#ddd 1px solid;border-left:#ddd 1px solid;padding-bottom:20px;background-color:#eee;margin:15px auto;padding-left:20px;padding-right:20px;border-top:#ddd 1px solid;border-right:#ddd 1px solid;padding-top:20px">' +
         comment.get('comment') +
         '</div>' +
-        '<p style="font-size: 14px;"><span><a style="text-decoration: none;color: #87daff" target="_blank" href="' +
+        '<p style="font-size: 14px;"><span><a style="text-decoration: none;color: #FF5722" target="_blank" href="' +
         process.env.SITE_URL +
         '">persilee\'s blog</a> 双手呈上~</span ></p>' +
         '<div style="text-align: center;"><span><img src="https://cdn.lishaoy.net/images/mail-bottom.png" alt="hr" style="width:100%;max-height:30px;margin:5px auto 5px auto;display: block;"/><a style="text-transform: uppercase; text-decoration: none; font-size: 14px; border: 2px solid #6c7575; color: #2f3333; padding: 10px; display: inline-block; margin: 10px auto 0;"target="_blank" href="' +
@@ -58,18 +57,17 @@ exports.notice = (comment) => {
 
 exports.send = (currentComment, parentComment) => {
     let emailSubject = '👉 叮咚！[' + process.env.SITE_NAME + '] 上的留言有了回应';
-    let emailContent = '<div style="background: white; max-width:740px; margin: auto auto; border-radius: 5px; border:#87daff 1px solid; overflow: hidden; -webkit-box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.12); box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.18);">' +
+    let emailContent = '<div style="background: white; max-width:740px; margin: 10px auto; border-radius: 5px; border:#FF5722 1px solid; overflow: hidden; -webkit-box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.12); box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.18);">' +
         '<a href="#">' +
         '<img height="auto" style="width:140%;max-width:740px;z-index: 666;" src="https://cdn.lishaoy.net/images/boy.png" />' +
         '</a>' +
         '<div style="padding: 5px 20px;">' +
-        '<div style="position: relative; color: white; float: left; z-index: 999; background: #87daff; padding: 5px 30px; margin: -25px auto 0 ; box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.30)">' +
-        '<span>Dear ' + parentComment.get('nick') +
-        '</span>' +
+        '<div style="position: relative; color: white; float: left; z-index: 999; background: #FF5722; padding: 5px 30px; margin: -25px auto 0 ; box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.30)">' +
+        'Dear ' + parentComment.get('nick') +
         '</div>' +
         '<span><br / ></span>' +
         '<h3> <span > 您有一条来自' +
-        '<a style="text-decoration: none;color: #87daff" target="_blank" href="' +
+        '<a style="text-decoration: none;color: #FF5722" target="_blank" href="' +
         process.env.SITE_URL +
         '">' + currentComment.get('nick') + '</a>的回复</span>' +
         '</h3>' +
@@ -81,10 +79,10 @@ exports.send = (currentComment, parentComment) => {
         '</div>' +
         '<p style="font-size: 14px;">' +
         '<span>' + currentComment.get('nick') + ' 给您的回复如下：</span></p>' +
-        '<div style="border-bottom:#ddd 1px solid;border-left:#ddd 1px solid;padding-bottom:20px;background-color:#eee;margin:15px 0px;padding-left:20px;padding-right:20px;border-top:#ddd 1px solid;border-right:#ddd 1px solid;padding-top:20px">' +
+        '<div style="border-bottom:#ddd 1px solid;border-left:#ddd 1px solid;padding-bottom:20px;background-color:#eee;margin:15px auto;padding-left:20px;padding-right:20px;border-top:#ddd 1px solid;border-right:#ddd 1px solid;padding-top:20px">' +
         currentComment.get('comment') +
         '</div>' +
-        '<p style="font-size: 14px;"><span><a style="text-decoration: none;color: #87daff" target="_blank" href="' +
+        '<p style="font-size: 14px;"><span><a style="text-decoration: none;color: #FF5722" target="_blank" href="' +
         process.env.SITE_URL +
         '">persilee\'s blog</a> 双手呈上~</span ></p>' +
         '<div style="text-align: center;"><span><img src="https://cdn.lishaoy.net/images/mail-bottom.png" alt="hr" style="width:100%;max-height:30px;margin:5px auto 5px auto;display: block;"/><a style="text-transform: uppercase; text-decoration: none; font-size: 14px; border: 2px solid #6c7575; color: #2f3333; padding: 10px; display: inline-block; margin: 10px auto 0;"target="_blank" href="' +
